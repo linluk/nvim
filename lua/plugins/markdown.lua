5751f52830400d2c7fae1bc0e872d20ce4bf64d5
Lukas Singer <lukas42singer@gmail.com>
2024-08-09 02:12:42 +0200

    found an awesome markdown plugin (now uing nerdfonts (btw.)

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,13 @@
 return {
-    'preservim/vim-markdown',
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }
+}
+    --[['preservim/vim-markdown',
     ft = 'markdown',
     config = function()
         vim.g.vim_markdown_fenced_languages = { 'python', 'c', 'javascript', 'html', 'css' }
+        vim.g.vim_markdown_folding_disabled = true
+        vim.g.vim_markdown_no_default_key_mappings = true
     end
-}
+}--]]
