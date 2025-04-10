185bcf76a25bf2c1b30a955ce1ead1688666c5e0
Lukas Singer <lukas42singer@gmail.com>
2025-04-10 11:09:46 +0200

    ?

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
     build = ":TSUpdate",
     config = function ()
         local configs = require("nvim-treesitter.configs")
-
         configs.setup({
             ensure_installed = { "c", "c_sharp", "lua", "vimdoc", "markdown", "markdown_inline", "html", "python" },
             sync_install = false,
